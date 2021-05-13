6d4b688e127a56e72863d5b7834cc0a44fa92621
Nhi Nguyen Quynh <69132739+nhing17899@users.noreply.github.com>
2021-05-13 10:25:46 +0700

    Delete Project1 directory

--- Project1/src/CaveContents.java
+++
@@ -1,8 +0,0 @@
-/**
- * Enumeration class CaveContents - the possible contents of a "Hunt the Wumpus" cave.
- *   @author Dave Reed
- *   
- */
-public enum CaveContents {
-    EMPTY, WUMPUS, BATS, PIT
-}
